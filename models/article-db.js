60b939b34bb871e50737479e631072d90b237488
chenshuanglin <1026624591@qq.com>
2014-12-12 04:30:03 +0800

    更改文章为对象

--- models/article-db.js
+++ models/article-db.js
@@ -15,33 +15,74 @@ connection.connect(function(err){
     console.log('connected as id ' + connection.threadId);
 });
 
+/*
+*定义一个article的文章对象，处理文章的增删改查操作
+*@author:chenshuanglin
+*@date 2014-12-11
+*/
+
+var Article = function(article)
+{
+    this.id = article.id;
+    this.title = article.title;
+    this.content = article.content;
+    this.mydate = article.mydate;
+    this.mytype = article.mytype;
+}
+
+module.exports = Article;
+
 
 //写入mysql文章记录
-var insert = function(title,content,mytype)
+Article.prototype.save = function(callback)
 {
     var now_date = new Date();
     console.log(now_date.toLocaleString());
-    var post = {title: title, content: content , mydate:now_date.toLocaleString(),type:mytype};
+    var post = {title: this.title, content: this.content , mydate:now_date.toLocaleString(),type: this.mytype};
     connection.query('insert into article set ?',post,function(err,result){
+        if(err) {
+            console.log('insert article Error: '+ err.message);
+            return;
+        }
         console.log(result);
+        callback(err,result);
     })
 }
 
-insert("chenshuanglin1", "我是无敌的","默认的");
+//insert("chenshuanglin1", "我是无敌的","默认的");
 //删除指定的文章记录
-var mydelete = function(article_id)
+Article.prototype.delete = function(article_id,callback)
 {
     var sql = 'delete from article where id = ?';
     connection.query(sql,[article_id],function(err,result){
+        if(err) {
+            console.log('delete article Error: '+err.message);
+        }
         console.log(result);
+        callback(err,result);
     });
 }
 
-mydelete(1);
+Article.prototype.showAll = function(callback)
+{
+    connection.query('SELECT * from article', function(err, rows, fields) {
+        if (err) {
+            console.log('showall article Error: '+err.message);
+         }
+        console.log(rows);
+        callback(err,rows,fields);
+    });
+}
 
-connection.query('SELECT * from article', function(err, rows, fields) {
-  if (err) throw err;
-  console.log(rows)
+var myArticle = new Article({ 
+    id: 1,
+    title: "show message",
+    content: "This is content body",
+    mydate: "",
+    mytype: "default"
+});
+
+myArticle.save(function (err, result){
 });
 
 connection.end();
