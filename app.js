410144c5227a0bf3ce1632b8350034cd47fd9c70
chenshuanglin <1026624591@qq.com>
2014-12-14 00:13:43 +0800

    用巨幕控件展示文章列表页面的实现
    尝试用巨幕实现一下界面

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var myueditor = require('./routes/ueditor');
+var showArticle = require('./routes/showArticle');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/ueditor/ue',myueditor);
+app.use('/showArticle',showArticle);
 
 //创建服务
 var server = app.listen(3000,function(){
